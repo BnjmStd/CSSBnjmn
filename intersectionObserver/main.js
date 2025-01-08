ae9eee6e727f39e50ea6e0a8837634c69215bfe6
Benjaaaamín <benja_e_e@hotmail.com>
2025-01-08 20:00:15 -0300

    Implement Intersection Observer and lazy loading functionality across multiple components

---
+++ intersectionObserver/main.js
@@ -0,0 +1,47 @@
+// objeto intersection
+
+new IntersectionObserver()
+
+// requiere de 2 cosas, un objeto que requiere opciones
+
+const opcion = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0 // valor de intersección, puede ser un valor o una matriz 
+}
+
+// elemento root por defecto es la visibilidad del objeto que por defecto es el viewport
+
+
+// el segundo componente es el callback 
+// se ejecutara cuando haya un cambio en la intersección del eleemento observado con respecto al root
+
+// por defecto recibe un array 
+
+const callback = () => {
+
+}
+
+
+const callbackk = ( entries ) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+
+        }        
+    });
+}
+
+const observer1Element = new IntersectionObserver({
+    callback,
+    option
+})
+
+observer.observer1Element(element)
+
+
+
+// si son varios 
+
+elements.forEach(element => {
+    observer.observer1Element(element)
+});
