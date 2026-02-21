399e9a351ce67e1f402ede9eff3137d67f3a5f3c
bnjmn <benja_e_e@hotmail.com>
2026-02-21 14:09:04 -0300

    up

---
+++ 2026/infinityImageSlider/index.js
@@ -0,0 +1,14 @@
+const $prev = document.querySelector('.prev');
+const $next = document.querySelector('.next');
+
+$prev.addEventListener('click', () => {
+    const $$items = document.querySelectorAll('.deck');
+
+    document.querySelector('.cards').appendChild($$items[0]);
+});
+
+$next.addEventListener('click', () => {
+    const $$items = document.querySelectorAll('.deck');
+
+    document.querySelector('.cards').prepend($$items[$$items.length - 1]);
+});
